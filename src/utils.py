ff5fb6a4bbf439d4dacee7cb3f12fef37dd1a6df
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-06-27 13:42:21 +0200

    utils file is added

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+import yaml
+import joblib
+
+
+def dump(value=None, filename=None):
+    if (value is not None) and (filename is not None):
+        joblib.dump(value=value, filename=filename)
+
+    else:
+        print("Error: value or filename is None".capitalize())
+
+
+def load(filename=None):
+    if filename is not None:
+        return joblib.load(filename=filename)
+
+    else:
+        print("Filename cannot be none".capitalize())
+
+
+def config():
+    with open("./config.yml", "r") as file:
+        return yaml.safe_load(file)
